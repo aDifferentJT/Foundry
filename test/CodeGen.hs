5b5a3d0aba0039ffad1598a70f69c50a4f40f0bd
Jonathan Tanner <nixCodeX@users.noreply.github.com>
2019-07-25 23:38:27 +0100

    Move to stack

--- test/CodeGen.hs
+++ test/CodeGen.hs
@@ -3,13 +3,16 @@ module Main(main) where
 import Parser.Parser (parse)
 import Parser.LexerMonad (runLexer)
 
+import CodeGen.CodeGen (genCode)
+
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  fn <- head <$> getArgs
+  --fn <- head <$> getArgs
+  let fn = "examples/processor_4.fdry"
   ast <- runLexer parse <$> readFile fn
   case ast of
-    Left e     -> putStr e
-    Right ast' -> print ast'
+    Left e      -> putStr e
+    Right ast' -> putStr . genCode $ ast'
 
---
+++ test/Parser.hs
@@ -0,0 +1,16 @@
+module Main(main) where
+
+import Parser.Parser (parse)
+import Parser.LexerMonad (runLexer)
+
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  --fn <- head <$> getArgs
+  let fn = "examples/processor_4.fdry"
+  ast <- runLexer parse <$> readFile fn
+  case ast of
+    Left e     -> putStr e
+    Right ast' -> print ast'
+
