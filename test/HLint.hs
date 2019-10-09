68ce0f525c3ef54a53db9f7501ff75cdd7a1b44d
Jonathan Tanner <jonathan@mljp.me.uk>
2019-10-09 17:19:46 +0100

    Remove dependency on hlint-test

--- test/HLint.hs
+++ test/HLint.hs
@@ -1,1 +1,14 @@
-{-# OPTIONS_GHC -F -pgmF hlint-test #-}
+module Main (main) where
+
+import Control.Monad (unless)
+import System.Exit (exitFailure)
+import System.IO (putStrLn)
+
+import Language.Haskell.HLint (hlint)
+
+main :: IO ()
+main = do
+    putStrLn "" -- less confusing output, test-framework does this too
+    hints <- hlint ["."]
+    unless (null hints) exitFailure
+
